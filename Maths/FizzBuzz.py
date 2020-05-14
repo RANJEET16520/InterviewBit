658d9394b0edddc4212855b11c0d3ed2a0b796d5
RANJEET16520 <ranjeetwalia2000@gmail.com>
2020-05-14 10:20:26 +0530

    Create FizzBuzz.py

---
+++ Maths/FizzBuzz.py
@@ -0,0 +1,15 @@
+def FizzBuzz(A):
+	ans = []
+	for i in range(1,A+1):
+		if i%3==0 and i%5==0:
+			ans.append('FizzBuzz')
+		elif i%3==0:
+			ans.append('Fizz')
+		elif i%5==0:
+			ans.append('Buzz')
+		else:
+			ans.append(i)
+	return ans
+
+n = int(input())
+print(FizzBuzz(n))
