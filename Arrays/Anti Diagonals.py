1ad1e9b5f50107aa04e5ddcbf37b877e7bd2fd40
RANJEET16520 <ranjeetwalia2000@gmail.com>
2020-05-17 18:42:16 +0530

    Update Anti Diagonals.py

--- Arrays/Anti Diagonals.py
+++ Arrays/Anti Diagonals.py
@@ -1,5 +1,33 @@
 # https://www.interviewbit.com/problems/anti-diagonals/
 
+'''
+Problem:
+Give a N*N square matrix, return an array of its anti-diagonals. Look at the example for more details.
+
+Example:
+Input: 	
+1 2 3
+4 5 6
+7 8 9
+Return the following :
+[ 
+  [1],
+  [2, 4],
+  [3, 5, 7],
+  [6, 8],
+  [9]
+]
+
+Input : 
+1 2
+3 4
+Return the following  : 
+[
+  [1],
+  [2, 3],
+  [4]
+]
+'''
 def AntiDiagonals(A):
 	mat = []
 	if len(A) == 1:
