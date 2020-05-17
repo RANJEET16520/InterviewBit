eba3b734ec49f31eb2a8a695a2ec6437c4c3ffc6
RANJEET16520 <ranjeetwalia2000@gmail.com>
2020-05-17 18:40:13 +0530

    Update AddOnetToAList.py

--- Arrays/AddOnetToAList.py
+++ Arrays/AddOnetToAList.py
@@ -1,4 +1,16 @@
 # https://www.interviewbit.com/problems/add-one-to-number/
+
+'''
+Problem:
+Given a non-negative number represented as an array of digits,
+add 1 to the number ( increment the number represented by the digits ).
+The digits are stored such that the most significant digit is at the head of the list.
+
+Example:
+If the vector has [1, 2, 3]
+the returned vector should be [1, 2, 4]
+as 123 + 1 = 124.
+'''
 def plusOne(A):
 	x = 1
 	for i in range(len(A)-1, -1, -1):
